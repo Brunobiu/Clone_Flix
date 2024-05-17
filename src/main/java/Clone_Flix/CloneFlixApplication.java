1d55c47ee256b31e9bcf29ca19a44e8608be79a6
bruno santana <bruno17henrique@gmail.com>
2024-05-16 23:48:26 -0300

    Implementando classe model e Conf. Performace

--- src/main/java/Clone_Flix/CloneFlixApplication.java
+++ src/main/java/Clone_Flix/CloneFlixApplication.java
@@ -2,12 +2,22 @@ package Clone_Flix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "Clone_Flix_model")
+@ComponentScan(basePackages = {"Clone.*"})
+@EnableJpaRepositories(basePackages = {"Clone_Flix_repository"})
+@EnableTransactionManagement
 public class CloneFlixApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloneFlixApplication.class, args);
 	}
 
+	
+	// Aula Configurações de performance e teste de CRUD -33:02
 }
---
+++ src/main/java/Clone_Flix_model/ModelLogin.java
@@ -0,0 +1,95 @@
+package Clone_Flix_model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Model_Login")
+@SequenceGenerator(name = "seq_Model_Login", sequenceName = "seq_Model_Login", allocationSize = 1, initialValue = 1)
+public class ModelLogin implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_Model_Login")
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nomeSobrenome;
+	
+	@Column(unique = true, nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String senha; // A senha será gerada automaticamente
+	
+	@Column(nullable = false)
+	private String cpf;
+	
+	@Column(nullable = false)
+	private String 	celular;
+	
+	
+	
+	/*SET E GET*/
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeSobrenome() {
+		return nomeSobrenome;
+	}
+
+	public void setNomeSobrenome(String nomeSobrenome) {
+		this.nomeSobrenome = nomeSobrenome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	
+	
+	
+	
+	
+
+}
